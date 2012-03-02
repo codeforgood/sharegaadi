60d8dbce211be04428ba56bcde8f612b77c64026
Sadish Ravi <sadish.r@gmail.com>
2012-03-01 22:23:12 -0500

    Adding HTML5 Geolocation to place the user on loading

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,25 @@
-function initialize() {
+function initialize() {	
 	var Chennai = new google.maps.LatLng(13.0604220, 80.2495830);
+	var initialLocation = Chennai;
+	
 	var myOptions = {
-	  center: Chennai,
 	  zoom: 15,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
-		myOptions);
+		myOptions);	
+	
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		  initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+		  map.setCenter(initialLocation);
+		}, function() {
+		  map.setCenter(initialLocation);
+		});
+	}else {
+		map.setCenter(initialLocation);
+	}
 }
 	  	  
 $(document).ready(function(){
